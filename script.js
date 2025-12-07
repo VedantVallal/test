a9f083dbdc553372d65d5f159f40999a23b39a82
VedantVallal <vedantvallal11@gmail.com>
2025-12-07 18:30:42 +0530

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+    function Person(name) {
+    console.log(name)
+    };
+
+    const p = new Person("Vedant"); 
+
+    for(let i =0;i<5;i++)
+    {
+        console.log(`Hello ${i}`)
+        if(i==3)break;
+    }
